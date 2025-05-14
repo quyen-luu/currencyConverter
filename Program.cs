7cf03b296a4049e4981ed265d2166b1c0ad1cecb
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-14 15:16:21 +0700

    Add content

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        double rate = 25964.48;
+        Console.Write("United States Dollar: ");
+        bool isValid = double.TryParse(Console.ReadLine(), out double USD);
+        if (!isValid)
+        {
+            Console.WriteLine("Dữ liệu nhập không đúng.");
+            return;
+        }
+        double VND = USD * rate;
+        Console.Write("Vietnamese dong: " + VND);
+    }
+}
